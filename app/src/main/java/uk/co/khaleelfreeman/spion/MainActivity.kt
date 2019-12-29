a0f1e58b8f3b1dc36e18a366064a6a308aeaaa48
khf01 <khaleel.freeman@sky.uk>
2019-12-29 21:02:36 +0000

    networking

--- app/src/main/java/uk/co/khaleelfreeman/spion/MainActivity.kt
+++ app/src/main/java/uk/co/khaleelfreeman/spion/MainActivity.kt
@@ -1,15 +1,10 @@
 package uk.co.khaleelfreeman.spion
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,39 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val viewManager = LinearLayoutManager(this)
-        val viewAdapter = ArticleAdapter(dummyData, ArticleViewHolder)
 
-        recycler_view.apply {
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = viewAdapter
-        }
+        val model = ViewModelProviders.of(this)[MainActivityViewModel::class.java]
+        model.getArticles(ArticleRepository()).observe(this, Observer<Array<Article>> { articles ->
+            val viewAdapter = ArticleAdapter(articles, ArticleViewHolder)
+            recycler_view.apply {
+                setHasFixedSize(true)
+                layoutManager = viewManager
+                adapter = viewAdapter
+            }
+        })
     }
 }
-
-
-val dummyData = arrayOf(
-    Article("Example Title could include anything here just make it a little longer", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("Example Title could include anything here", timeStamp = "12/12/2019"),
-    Article("title 2"),
-    Article("title 3"),
-    Article("title 4"),
-    Article("title 5")
-)
-
-data class Article(
-    val title: String = "",
-    val image: String = "",
-    val timeStamp: String = "",
-    val url: String = ""
-)
