6792d20dde9b8ddb2811b469e28c0e5f14712b21
khf01 <khaleel.freeman@sky.uk>
2019-12-31 12:04:11 +0000

    add toolbar animation for scrolling events

--- app/src/main/java/uk/co/khaleelfreeman/spion/MainActivity.kt
+++ app/src/main/java/uk/co/khaleelfreeman/spion/MainActivity.kt
@@ -2,12 +2,13 @@ package uk.co.khaleelfreeman.spion
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         toolbar.inflateMenu(R.menu.menu_item)
+
         toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.info -> {
@@ -28,6 +30,7 @@ class MainActivity : AppCompatActivity() {
                 else -> super.onOptionsItemSelected(it)
             }
         }
+
         viewAdapter = ArticleAdapter(emptyArray(), ArticleViewHolder)
         viewManager = LinearLayoutManager(this)
         recycler_view.apply {
@@ -35,6 +38,7 @@ class MainActivity : AppCompatActivity() {
             layoutManager = viewManager
             adapter = viewAdapter
         }
+
         model.getArticles(ArticleRepository).observe(this, Observer<Array<Article>> { articles ->
             if (articles.isNotEmpty()) {
                 viewAdapter.articles = emptyArray()
@@ -45,6 +49,17 @@ class MainActivity : AppCompatActivity() {
             viewAdapter.articles = articles
             viewAdapter.notifyItemRangeInserted(0,articles.size)
         })
+
+        recycler_view.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0) {
+                    app_bar_layout.elevation = 20f
+                } else {
+                    app_bar_layout.elevation = 0f
+                }
+            }
+        })
+
     }
 
     private fun showInfo() {
